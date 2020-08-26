12ec15ac1ba9893c0032203006f45c4fa3f41dcd
Beatriz Viseu Linhares <bivilin@gmail.com>
2020-08-26 20:46:27 -0300

    Revert "Circle inflating"

--- circle/RoundManager.swift
+++ circle/RoundManager.swift
@@ -8,34 +8,23 @@
 
 import Foundation
 
-protocol ViewSizeRenderer: class {
-    func updateViewSize(newSize: Float)
-    func resetViewSizeToMinimum(minimumSize: Float)
-    func updateTimeDurationLabel(newDuration: Float)
-}
-
 class RoundManager {
-    var minViewSize: Float
-    var maxViewSize: Float
-    var currentSize: Float
+    var minCircleSize: Int
+    var maxCircleSize: Int
+    var currentSize: Double
     var timer : Timer?
-    var currentDurationInSeconds: Float?
 
     var isBreathing: Bool = false
     var isInflating: Bool = false
 
-    weak var viewSizeRenderer: ViewSizeRenderer?
-
-    init(minViewSize: Float, maxViewSize: Float, viewSizeRenderer: ViewSizeRenderer) {
-        self.minViewSize = minViewSize
-        self.maxViewSize = maxViewSize
-        self.currentSize = Float(minViewSize)
-        currentDurationInSeconds = 0
-        self.viewSizeRenderer = viewSizeRenderer
+    init(minCircleSize: Int, maxCircleSize: Int) {
+        self.minCircleSize = minCircleSize
+        self.maxCircleSize = maxCircleSize
+        self.currentSize = Double(minCircleSize)
     }
 
-    func getNewViewSize(isInflating: Bool) -> Float {
-        var sizeRate: Float = 1
+    func updatesCircleSize(currentSize: Double, isInflating: Bool) -> Double {
+        var sizeRate: Double = 1
 
         if isInflating {
             sizeRate = 1.005
@@ -43,49 +32,19 @@ class RoundManager {
             sizeRate = 0.999
         }
 
-        let newSize = self.currentSize * sizeRate
+        let newSize = currentSize * sizeRate
         return newSize
     }
 
-    func startRound() {
-        self.isBreathing = true
-        self.isInflating = true
-
+    func startRound(completion: @escaping (Double) -> Void) {
         self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (Timer) in
+            var newSize: Double
 
-            // Updates Time Passed
-            if let duration = self.currentDurationInSeconds {
-                self.currentDurationInSeconds = duration + 0.01
-            }
+            newSize = self.updatesCircleSize(currentSize: self.currentSize, isInflating: self.isInflating)
+            self.currentSize = newSize
 
-            // Saves New View Size
-            self.currentSize = self.getNewViewSize(isInflating: self.isInflating)
-
-            // Asks for UI to change the size
-            self.viewSizeRenderer?.updateViewSize(newSize: self.currentSize)
-            self.viewSizeRenderer?.updateTimeDurationLabel(newDuration: self.currentDurationInSeconds ?? 0)
+            completion(newSize)
 
         })
-    }
-
-    func finishRoud() {
-        self.viewSizeRenderer?.resetViewSizeToMinimum(minimumSize: self.minViewSize)
-        self.viewSizeRenderer?.updateTimeDurationLabel(newDuration: 0)
-        self.currentSize = self.minViewSize
-        self.timer?.invalidate()
-    }
-
-    func finishInhaling() {
-        let inhaleDuration = self.currentDurationInSeconds
-        self.currentDurationInSeconds = 0
-        self.isInflating = false
-        print("Inspirou por \(String(describing: inhaleDuration)) segundos.")
-    }
-
-    func finishExhaling() {
-        let exhaleDuration = self.currentDurationInSeconds
-        self.currentDurationInSeconds = 0
-        self.isInflating = true
-        print("Expirou por \(String(describing: exhaleDuration)) segundos.")
     }
 }
--- circle/ViewController.swift
+++ circle/ViewController.swift
@@ -6,16 +6,45 @@
 //  Copyright © 2020 Beatriz Viseu Linhares. All rights reserved.
 //
 
+// TODO: expiração: círculo diminuindo quando tira o dedo -- state.ended do longpress
+
+// TODO: definir o que acontece quando o usuário aperta de novo
+//  --> círculo volta pro ponto inicial e começa a crescer de novo
+
+// TODO: como visualizar meta da expiração?
+
+// TODO: como salvar as respirações?
+// Array de um struct?
+
 
 import UIKit
+
+//protocol SaveRespiration {
+//    func addRespirationToRound(respiration: Respiration)
+//}
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var circleView: UIView!
-    @IBOutlet weak var diameterContraint: NSLayoutConstraint!
-    @IBOutlet weak var durationLabel: UILabel!
 
-    var roundManager: RoundManager?
+    // Changing this value allows circle inflation/deflation
+    @IBOutlet weak var diameterContraint: NSLayoutConstraint!
+
+    // End Button
+    @IBOutlet weak var buttonBackground: UIView!
+    @IBOutlet weak var buttonText: UIButton!
+
+    // Breathe Duration Label
+    @IBOutlet weak var timeLabel: UILabel!
+
+    var timer : Timer?
+    var time: Double = 0.0
+
+    var roundManager = RoundManager(minCircleSize: 30, maxCircleSize: 300)
+
+    // String to be outputed by the end of the Round
+//    var outputRespiration: String = ""
+    var respirationId: Int = 0
 
     // Hapict Feeddback
     let impact = UIImpactFeedbackGenerator()
@@ -23,11 +52,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Creates RoundManager
-        roundManager = RoundManager(minViewSize: 30, maxViewSize: 300, viewSizeRenderer: self)
+        // Visual design for button
+//        self.buttonBackground.layer.cornerRadius = self.buttonBackground.layer.cornerRadius / 1.5
+
+        //Button Text
+//        buttonText.titleLabel?.text = "concluir"
 
         // Make view look like a circle
         self.circleView.layer.cornerRadius = self.diameterContraint.constant / 2
+
+        // Idle Setup
+        timeLabel.text = ""
 
         // Gestures
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressed))
@@ -36,19 +71,24 @@ class ViewController: UIViewController {
 
     @objc func longPressed(gesture: UILongPressGestureRecognizer) {
 
-        guard let roundManager = self.roundManager else {return}
-
         if gesture.state == .began {
+
+            if roundManager.isBreathing {
+                //saveRespiration()
+                time = 0
+            } else {
+                roundManager.isBreathing = true
+                roundManager.startRound() { (newSize) in
+                    self.diameterContraint.constant = CGFloat(newSize)
+                    self.circleView.layer.cornerRadius = self.diameterContraint.constant / 2
+                }
+            }
+
             // Hapict Feeddback
             impact.impactOccurred()
 
-            if roundManager.isBreathing {
-                // Activates inhale
-                roundManager.finishExhaling()
-            } else {
-                // Starts breath session
-                roundManager.startRound()
-            }
+            // Activates inhale
+            roundManager.isInflating = true
         }
 
         if gesture.state == .ended {
@@ -56,42 +96,16 @@ class ViewController: UIViewController {
             impact.impactOccurred()
 
             // Activates exhale
-            roundManager.finishInhaling()
+            roundManager.isInflating = false
         }
     }
 
-    // Stops circle movement
+
     @IBAction func stopButton(_ sender: Any) {
-        // Stops timer
-        if let roundManager = self.roundManager {
-            roundManager.isBreathing = false
-            roundManager.finishRoud()
-        }
-    }
-
-}
-
-extension ViewController: ViewSizeRenderer {
-    func updateViewSize(newSize: Float) {
-        let sizeInFloat = CGFloat(truncating: newSize as NSNumber)
-        self.diameterContraint.constant = sizeInFloat
-        self.circleView.layer.cornerRadius = self.diameterContraint.constant / 2
-    }
-
-    func resetViewSizeToMinimum(minimumSize: Float) {
-        self.circleView.layoutIfNeeded() // force any pending operations to finish
-
-        UIView.animate(withDuration: 1, animations: { () -> Void in
-            self.diameterContraint.constant = CGFloat(minimumSize)
-            self.circleView.layer.cornerRadius = self.diameterContraint.constant / 2
-            self.view.layoutIfNeeded()
-        })
-    }
-
-    func updateTimeDurationLabel(newDuration: Float) {
-        let roundedDuration = Int(newDuration)
-        let stringDuration = String(roundedDuration)
-        self.durationLabel.text = stringDuration
+        timer?.invalidate()
+//        print(outputRespiration)
+//        let alert = UIAlertController(title: "Resumo da respiração", message: outputRespiration, preferredStyle: .alert)
+//        self.present(alert, animated: true, completion:  nil)
     }
 
 }
